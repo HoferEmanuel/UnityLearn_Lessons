b549f8818053a7c8c0369d0fcd75345ebd25466d
Emanuel Hofer <102549117+HoferEmanuel@users.noreply.github.com>
2022-08-16 10:52:04 +0200

    new160822

--- Junior_Programmer/Unit01/Lesson1.4/PlayerConroller.cs
+++ Junior_Programmer/Unit01/Lesson1.4/PlayerConroller.cs
@@ -5,17 +5,16 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float vehicleSpeed = 15;
     [SerializeField] float turnSpeed = 0;
     [SerializeField] float horizontalMovement = 0, verticalInput = 0;
-  
-    private void Start()
-    {
-    }
-    
+
     private void Update()
     {
+        //playerInput
         horizontalMovement = Input.GetAxis("Horizontal");
         verticalMovement = Input.GetAxis("Vertical");
     
+        //move vehicle back and forth
         transform.Translate(Vector3.forward * time.deltaTime * vehicleSpeed * verticalInput);
-        transform.Translate(Vector3.right * time.deltaTime * turnSpeed * horizontalMovment);
+        //turn vehicle
+        transform.Rotate(Vector3.up, time.deltaTime * turnSpeed * horizontalMovement);
     }
 }
